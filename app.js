c35bf6ab38e9064726db8670b13d87803d14ff35
daimin <vagasnail@gmail.com>
2012-09-29 18:04:13 +0800

    change jade to ejs.

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || config.port);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'html');
+  app.engine('.html', require('ejs').__express);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser({uploadDir:config.upload_dir}));
